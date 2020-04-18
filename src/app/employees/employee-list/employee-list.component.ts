d93c2db2b3520e0bdedb8ed01b5ae6f641e2cf3f
Issah <issahsakasaka@gmail.com>
2020-04-18 11:36:31 -0400

    first commit

---
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EmployeeService } from '../../shared/employee.service';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { DepartmentService } from '../../shared/department.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { EmployeeComponent } from '../employee/employee.component';
+import { NotificationService } from 'src/app/shared/notification.service';
+import { DialogService } from 'src/app/shared/dialog.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  constructor(private service: EmployeeService,
+              private departmentService: DepartmentService,
+              private dialog: MatDialog,
+              private notificationService: NotificationService,
+              private dialogService: DialogService) { }
+
+  listData: MatTableDataSource<any>;
+  displayedColumns: string[] = ['fullName', 'email', 'mobile', 'city', 'departmentName', 'actions'];
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  searchKey: string;
+
+  ngOnInit() {
+    this.service.getEmployees().subscribe(
+      list => {
+        const array = list.map(item => {
+          const departmentName = this.departmentService.getDepartmentName(item.payload.val().department);
+          return {
+            $key: item.key,
+            departmentName,
+            ...item.payload.val()
+          };
+        });
+        this.listData = new MatTableDataSource(array);
+        this.listData.sort = this.sort;
+        this.listData.paginator = this.paginator;
+        this.listData.filterPredicate = (data, filter) => {
+          return this.displayedColumns.some(ele => {
+            return ele != 'actions' && data[ele].toLowerCase().indexOf(filter) != -1;
+          });
+        };
+      });
+  }
+
+  onSearchClear() {
+    this.searchKey = '';
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    this.listData.filter = this.searchKey.trim().toLowerCase();
+  }
+
+
+  onCreate() {
+    this.service.initializeFormGroup();
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '70%';
+    this.dialog.open(EmployeeComponent, dialogConfig);
+  }
+
+  onEdit(row) {
+    this.service.populateForm(row);
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '70%';
+    this.dialog.open(EmployeeComponent, dialogConfig);
+  }
+  
+  onDelete($key: string){
+    this.dialogService.openConfirmDialog('Are you sure you want to delete this record?')
+    .afterClosed().subscribe(res =>{
+      if(res){
+        this.service.deleteEmployee($key);
+        this.notificationService.warn('! Deleted successfully');
+      }
+    });
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDdylIiYXhhrIiHxE0DkdjrMSrG_1PwZH4",
+    authDomain: "amdb-29129.firebaseapp.com",
+    databaseURL: "https://amdb-29129.firebaseio.com",
+    projectId: "amdb-29129",
+    storageBucket: "amdb-29129.appspot.com",
+    messagingSenderId: "147794197103"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
